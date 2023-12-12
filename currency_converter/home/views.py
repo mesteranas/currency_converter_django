465a5e8af2c6ecdbbf090abd9c7fbf75df2b5288
mesteranas <135442710+mesteranas@users.noreply.github.com>
2023-12-12 21:43:42 +0200

    12/12/2023

---
+++ currency_converter/home/views.py
@@ -0,0 +1,11 @@
+from . import google_currency
+from django.shortcuts import render
+
+# Create your views here.
+def home_(r):
+    return render(r,"home.html")
+def currency(r):
+    result=""
+    if r.method=="POST":
+        result=google_currency.convert(r.POST["From"],r.POST["To"],int(r.POST["Value"]))
+    return render(r,"currency.html",{"currencies":google_currency.CODES.items(),"Result":str(result)})
