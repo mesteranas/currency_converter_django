21b05a4ba10894a3c0e7ee38eec26e038644e08f
mesteranas <135442710+mesteranas@users.noreply.github.com>
2023-12-13 12:51:50 +0200

    13/12/2023

--- currency_converter/home/urls.py
+++ currency_converter/home/urls.py
@@ -3,4 +3,6 @@ from .views import *
 urlpatterns=[
     path("",home_,name="homePage"),
     path("currency/",currency,name="Currency"),
+    path("contect/",Contect,name="contect"),
+    path("about/",about,name="about"),
 ]
--- currency_converter/home/views.py
+++ currency_converter/home/views.py
@@ -9,3 +9,7 @@ def currency(r):
     if r.method=="POST":
         result=google_currency.convert(r.POST["From"],r.POST["To"],int(r.POST["Value"]))
     return render(r,"currency.html",{"currencies":google_currency.CODES.items(),"Result":str(result)})
+def Contect(r):
+    return render(r,"contect.html")
+def about(r):
+    return render(r,"about.html")
